647f4213e1f2df7fa8606910ec50428a0e02c239
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-18 22:37:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,21 @@
 def new_hash
-	empty_hash: {
+	empty_hash => {
 
 	}
 end
 
 def actor
-	actor: {:name => "Dwayne The Rock Johnson"}
+	actor => {:name => "Dwayne The Rock Johnson"}
 
 end
 
 def monopoly
-	monopoly: {:railroads => {}}
+	monopoly => {:railroads => {}}
 
 end
 
 def monopoly_with_second_tier
-	monopoly:  {:railroads => {
+	monopoly =>  {:railroads => {
 		:pieces => 4,
 		:names => {},
 		rent_in_dollars: {}
@@ -25,7 +25,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = {:railroads => {
+	monopoly => {:railroads => {
 		:pieces => 4,
 		:names => {
 			:reading_railroad => {},
@@ -45,7 +45,7 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-	monopoly = {:railroads => {
+	monopoly => {:railroads => {
 		:pieces => 4,
 		:names => {
 			:reading_railroad => {},
